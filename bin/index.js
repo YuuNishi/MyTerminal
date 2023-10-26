3bd4ba75b06eb3ab3972a5bb6f49d2ed49972155
Nishi <84825027+YuuNishi@users.noreply.github.com>
2023-10-26 15:03:14 -0300

    Welcome message and help

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,12 @@
 #! /usr/bin/env node
+const yargs = require("yargs");
 console.log("Hello, World!");
-console.log("****************");
-console.log("YUUU terminal");
-console.log("****************");
+console.log("__   __ _   _  _   _ ");
+console.log("\\ \\ / /| | | || | | |");
+console.log(" \\ V / | |_| || |_| |");
+console.log("  |_|   \\___/  \\___/")
 console.log("I will be alive soon...");
+
+const usage = "\nUsage: yuu <option>";
+const options = yargs .usage(usage) .option("l", {alias:"Info", describe: "List my infos", type: "boolean", demandOption
+: false }).help(true).argv;
