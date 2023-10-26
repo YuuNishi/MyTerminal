a8e9019d5c3be30fe96e09f18032438043c9db39
Nishi <84825027+YuuNishi@users.noreply.github.com>
2023-10-26 14:00:55 -0300

    created cli index and json

---
+++ bin/index.js
@@ -0,0 +1,6 @@
+#! /usr/bin/env node
+console.log("Hello, World!");
+console.log("****************");
+console.log("YUUU terminal");
+console.log("****************");
+console.log("I will be alive soon...");
